2d47cc05cd53fbdd18df6774ae4e61cee163718f
Jakub Szatkowski <mistrzswiata.salata@gmail.com>
2018-08-30 20:55:24 +0200

    corrections

--- INT2018/settings.py
+++ INT2018/settings.py
@@ -198,7 +198,11 @@ REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
     ],
-    'DEFAULT_FILTER_BACKENDS': ('django_filters.rest_framework.DjangoFilterBackend',)
+    'DEFAULT_FILTER_BACKENDS': ('django_filters.rest_framework.DjangoFilterBackend',),
+    'DEFAULT_RENDERER_CLASSES': (
+        'rest_framework.renderers.JSONRenderer',
+        'rest_framework.renderers.BrowsableAPIRenderer',
+    )
 }
 
 ASGI_APPLICATION = "INT2018.routing.application"
